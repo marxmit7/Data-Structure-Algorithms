f9a51861a4ca503ce5f08c6720bb6354e057e8f1
ami <amit2rockpower@gmail.com>
2017-10-17 14:37:08 +0530

    Topview tree

---
+++ tree/topview.c
@@ -0,0 +1,25 @@
+node *topview(node*root)
+{
+    int arr[100];
+    int i=0;
+    node * tmp;
+    tmp=root->left;
+    while(tmp!=NULL)
+    {
+        arr[i]=tmp->data;
+        i++;
+        tmp=tmp->left;
+    }
+    while(i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    tmp=root;
+
+    while(tmp!=NULL)
+    {
+        printf("%d ",tmp->data);
+        tmp=tmp->right;
+
+    }
+}
