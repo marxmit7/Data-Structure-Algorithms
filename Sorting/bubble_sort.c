de75330c43ea16c56cbffdbbcdb435427243ab0f
amit <amit2rockpower@gmail.com>
2017-11-11 14:26:46 +0530

    bubbleSort

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -1,15 +1,16 @@
+
 void bubblesort(int arr[],int n)
 {
     int tmp;
-    for(int i=0;i<n-1;i++)
+    for(int i=0;i<n-1;i++)// n-1 for since the last comparison is aleardy sorted
     {
-        for(int k=0;k<n-i-1;k++)
+        for(int k=0;k<n-i-1;k++)// n-i-1 just to leave the comparisons between the aleardy sorted elements
         {
-            if(arr[k]>arr[k+1])
+            if(arr[k]>arr[k+1]) //comparison
             {
-                tmp=arr[k];
-                arr[k]=arr[k+1];
-                arr[k+1]=tmp;
+                tmp=arr[k]; // temporarily assigning the greater value
+                arr[k]=arr[k+1]; // assigning the greater value
+                arr[k+1]=tmp // assigning the lower value
             }
         }
     }
